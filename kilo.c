5edb568e917c8cc26c72abbb9c9df4e77b9e3ecb
neelamber <nlm.1010@gmail.com>
2025-10-22 15:50:45 +0530

    added more abstraction to the keypress

--- kilo.c
+++ kilo.c
@@ -33,18 +33,31 @@ void enableRawMode() {
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+/*** input ***/
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+void editorProcessKeypress() {
+    char c = editorReadKey();
+
+    switch(c) {
+        case CTRL_KEY('q'):
+            exit(0);
+            break;
+    }
+}
+
 /*** init ***/
 int main() {
     enableRawMode();
     while(1){
-        char c = '\0';
-        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read"); 
-        if(iscntrl(c)){
-            printf("%d\r\n",c);
-        } else {
-            printf("%d ('%c')\r\n",c,c);
-        }
-        if(c == CTRL_KEY('q') ) break;
+        editorProcessKeypress();
     }
     disableRawMode();
     return 0;
