98b5e674a72e29bd11a31909812485acb52429c9
neelamber <nlm.1010@gmail.com>
2025-10-22 15:37:41 +0530

    basic raw mode enabled

---
+++ kilo.c
@@ -0,0 +1,51 @@
+/*** includes ***/
+#include<termios.h>
+#include<ctype.h>
+#include<stdio.h>
+#include<errno.h>
+#include<stdlib.h>
+#include <unistd.h>
+
+/*** defines ***/
+#define CTRL_KEY(k) ((k)& (0x1f))
+
+/*** data ***/
+struct termios org_termios;
+
+/*** terminal ***/
+void die(const char *s){
+    perror(s);
+    exit(1);
+}
+void disableRawMode() {
+   if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &org_termios) == -1) die("tcsetattr"); 
+}
+void enableRawMode() {
+    if(tcgetattr(STDIN_FILENO, &org_termios) == -1) die("tcsetattr");
+    atexit(disableRawMode);
+    struct termios raw = org_termios;
+    raw.c_iflag &= ~(BRKINT|INPCK|ISTRIP|IXON| ICRNL);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= ~(CS8);
+    raw.c_lflag &= ~(ECHO| ICANON| ISIG| IEXTEN);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] =1;
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+/*** init ***/
+int main() {
+    enableRawMode();
+    while(1){
+        char c = '\0';
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read"); 
+        if(iscntrl(c)){
+            printf("%d\r\n",c);
+        } else {
+            printf("%d ('%c')\r\n",c,c);
+        }
+        if(c == CTRL_KEY('q') ) break;
+    }
+    disableRawMode();
+    return 0;
+}
