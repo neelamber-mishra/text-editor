049302b70be1d57ea8caf37bb8696aca69b7bea3
neelamber <nlm.1010@gmail.com>
2025-11-30 18:12:23 +0530

    step 38

--- kilo.c
+++ kilo.c
@@ -1,8 +1,10 @@
 /*** includes ***/
+#include <asm-generic/ioctls.h>
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -43,21 +45,60 @@ void enableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
     die("tcsetattr");
 }
+char editorReadKey();
+int getCursorPosition(int* rows,int* cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+    while( i<sizeof(buf)-1){
+        if(read(STDOUT_FILENO, &buf[i], 1) != 1) break;
+        if(buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+    if(buf[0] != '\x1b' || buf[1] != '[' ) return -1;
+    if(sscanf(&buf[2], "%d;%d", rows,cols) != 2) return -1;
+    return 0;
+}
 int getWindowsSize(int *rows, int *cols) {
   struct winsize ws;
-  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+  if ( ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B",12) != 12) return -1;
+    return getCursorPosition(rows, cols);
   } else {
     *cols = ws.ws_col;
     *rows = ws.ws_row;
     return 0;
   }
 }
+/*** append buffer ***/
 
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend( struct abuf *ab, const char *s, int len){
+    char *new = realloc(ab->b, ab->len +len);
+
+    if(new == NULL ) return;
+    memcpy(&new[ab->len], s, len );
+    ab->b = new;
+    ab->len += len;
+}
+void abFree(struct abuf *ab){
+    free(ab->b);
+}
 /*** output ***/
 void editorDrawRows() {
   for (int y = 0; y < E.screenrows; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    write(STDOUT_FILENO, "~", 1);
+    if( y< E.screenrows -1){
+        write(STDOUT_FILENO, "\r\n", 2);
+        }
   }
 }
 
